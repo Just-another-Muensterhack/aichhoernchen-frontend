65f9d1e3f86f558f246236d5916f476c585c5b85
Felix Thape <felix.thape@gmail.com>
2025-09-26 17:58:06 +0200

    chore: update logo-text

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,12 +3,9 @@ import Image from "next/image";
 export const Header = () => {
     return (
         <header className={"flex-row-0 justify-between h-16 max-h-16 px-6 py-4"}>
-            <h1 className={"flex-row-2 justify-center items-center"}>
-                <Image src={"favicon.ico"} alt={"AIhörnchen Logo, ein Eichhörnchen"} width={32} height={32} />
-                <span className={"text-xl font-semibold"}>
-                    <span className={"text-primary text-2xl"}>AI</span>
-                    {"chhörnchen"}
-                </span>
+            <h1 className={"flex-row-1 justify-center items-center"} aria-label={"AIhörnchen Logo, ein Eichhörnchen"}>
+                <Image src={"favicon.ico"} alt={""} width={32} height={32} />
+                <Image src={"logo-text.png"} alt={""} width={150} height={28} />
             </h1>
             <nav className={"flex-row-0 items-center justify-center"}>
                 <ul className={"flex-row-4"}>
