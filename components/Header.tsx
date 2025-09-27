5ba826f8a6e9aeec79b03cffa07d43dc7ea202a8
Felix Thape <felix.thape@gmail.com>
2025-09-27 13:12:48 +0200

    feat: add map instructions

--- components/Header.tsx
+++ components/Header.tsx
@@ -43,7 +43,7 @@ export const Header = () => {
                 <div className={"relative w-10 h-5 bg-foreground/30 rounded-full"}>
                     <div
                         className={clsx(
-                            "absolute bg-primary w-5 h-5 rounded-full transition-all duration-300 left-0",
+                            "absolute bg-primary w-5 h-5 rounded-full transition-all duration-200 left-0",
                             {
                                 "translate-x-5": resolvedTheme === "dark",
                             }
